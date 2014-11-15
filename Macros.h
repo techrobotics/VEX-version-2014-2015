e0bf9c7dad623d2c30abf7d32f25299d23bf97ad
Michael <michael_9630@yahoo.com>
2014-11-15 09:43:36 -0800

    Change secArm starting angle

--- Macros.h
+++ Macros.h
@@ -78,7 +78,7 @@
 
 
 //to get degree of arms
-#define rawSecArmDegree (encoderDegrees(secArmSens, 60/12) - 53)
+#define rawSecArmDegree (encoderDegrees(secArmSens, 60/12) - 67)
 
 #define mainArmDegree (encoderDegrees(mainArmSens, 84/12) + 30)
 #define secArmDegree 90 + mainArmDegree - (-rawSecArmDegree)
